1c12e54162b691f359579604b17ba914c6df82e8
David FRANCOIS <david.francois@webflows.fr>
2014-04-21 22:12:19 +0200

    Small bugfix

--- lib/fix/protocol/messages/logon.rb
+++ lib/fix/protocol/messages/logon.rb
@@ -18,7 +18,7 @@ module Fix
         def validate(force = false)
           super(force)
           errors << "Encryption is not supported, the transport level should handle it" unless (encrypt_method == 0)
-          errors << "Heartbeat interval should be between 10 and 60 seconds"            unless heart_bt_int && heart_bt_int <= 60 && heart_bt_int >= 30
+          errors << "Heartbeat interval should be between 10 and 60 seconds"            unless heart_bt_int && heart_bt_int <= 60 && heart_bt_int >= 10
         end
 
       end

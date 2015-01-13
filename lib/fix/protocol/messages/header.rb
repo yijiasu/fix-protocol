43f83a28512e03da76ad77acb16f26aeb60b52a8
davout <david.francois@webflows.fr>
2015-01-13 12:04:32 +0100

    WIP

--- lib/fix/protocol/messages/header.rb
+++ lib/fix/protocol/messages/header.rb
@@ -4,6 +4,10 @@ require 'fix/protocol/unordered_part'
 module Fix
   module Protocol
     module Messages
+
+      #
+      # The header fields of a message
+      #
       class HeaderFields < UnorderedPart
 
         field :msg_type,        tag: 35,    required: true
@@ -11,8 +15,6 @@ module Fix
         field :target_comp_id,  tag: 56,    required: true
         field :msg_seq_num,     tag: 34,    required: true, type: :integer
         field :sending_time,    tag: 52,    required: true, type: :timestamp, default: proc { Time.now }
-        field :app_ver_id,      tag: 1128
-
 
       end
     end
